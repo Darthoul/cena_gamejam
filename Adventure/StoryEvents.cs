0c451c00f801284dca20cb28562acb1504318ee0
Darthoul <hessenrod@gmail.com>
2016-01-31 12:17:40 -0500

    Some stories...

--- Adventure/StoryEvents.cs
+++ Adventure/StoryEvents.cs
@@ -9,10 +9,15 @@ public class StoryEvents : MonoBehaviour {
 
 	public Transform speechBox;
 	public Transform endingBox;
+	public Transform transitionBox;
+	public Transform tableBox;
 
 	public static Speech currentSpeech;
 	public static Dialog currentDialog;
+	public static Transform currentTransition;
+	public static Transform currentTable;
 	public bool speaking = false;
+	public bool inTransition = false;
 	private float timer = 0;
 	// Use this for initialization
 	void Awake () {
@@ -20,10 +25,16 @@ public class StoryEvents : MonoBehaviour {
 		advLoader.LoadAll ();
 		speechBox = GameObject.Find ("SpeechBox").transform;
 		endingBox = GameObject.Find ("GameOverBox").transform;
+		transitionBox = GameObject.Find ("EpisodeBox").transform;
+		tableBox = GameObject.Find ("TableBox").transform;
 		SpeechArchive.Search ("s1").MoveToActive ();
 		SpeechArchive.Search ("s2").MoveToActive ();
 		SpeechArchive.Search ("s3").MoveToActive ();
 		LoadQuestSection (1, 7);
+		currentTransition = transitionBox.FindChild ("chapter1");
+		currentTable = transitionBox.GetChild (0);
+
+		inTransition = true;
 		SpeechPrompt ();
 	}
 
@@ -31,12 +42,12 @@ public class StoryEvents : MonoBehaviour {
 	
 	}
 	#region CallOnActivate 
-	public static void ChangeLevel (List<string> questParams) {
-		Debug.LogWarning (questParams [0]);
+	public void ChangeLevel (List<string> questParams) {
 		instance.endingBox.FindChild (questParams [0]).position = Vector3.zero;
 	}
-	public static void ChapterTransition (List<string> questParams) {
-
+	public void ChapterTransition (List<string> questParams) {
+		currentTransition = instance.transitionBox.FindChild (questParams [0]);
+		inTransition = true;
 	}
 	#endregion
 
@@ -58,6 +69,39 @@ public class StoryEvents : MonoBehaviour {
 				} else {
 					speaking = false;
 					SpeechPrompt ();
+				}
+			}
+		}
+		if (inTransition) {
+			if (!speaking) {
+				if (currentTransition.position != Vector3.zero) {
+					currentTransition.position = Vector3.zero;
+				}
+				timer += Time.deltaTime;
+				if (timer >= 3) {
+					currentTransition.localPosition = Vector3.zero;
+					currentTable.localPosition = Vector3.zero;
+					switch (currentTransition.name) {
+					case "chapter2":
+						SpeechArchive.Search ("s14").MoveToActive ();
+						SpeechArchive.Search ("s15").MoveToActive ();
+						currentTable = tableBox.GetChild(1);
+						break;
+					case "chapter3":
+						SpeechArchive.Search ("s68").MoveToActive ();
+						currentTable = tableBox.GetChild(2);
+						break;
+					case "chapter4":
+						SpeechArchive.Search ("s69").MoveToActive ();
+						SpeechArchive.Search ("s70").MoveToActive ();
+						currentTable = tableBox.GetChild(3);
+						break;
+					}
+					currentTable.position = Vector3.zero;
+					SpeechPrompt ();
+					currentTransition = null;
+					inTransition = false;
+					timer = 0;
 				}
 			}
 		}
